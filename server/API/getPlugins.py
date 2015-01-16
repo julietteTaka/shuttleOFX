82d21c03561a529856befc2d4168528f249d71dd
julietteTaka <juliettetuxbelin@gmail.com>
2015-01-16 19:33:46 +0100

    plugin : configParser used

--- server/API/getPlugins.py
+++ server/API/getPlugins.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python
-import logging
 from flask import Flask, jsonify
 from pyTuttle import tuttle
-from pluginsModule import runPlugin
+from ofxPlugins import analyze
+
+import logging
+import ConfigParser
+
 
 app = Flask(__name__, static_folder='', static_url_path='')
+configParser =  ConfigParser.RawConfigParser()
+configParser.read('ofxPlugins/configuration.conf')
 
 version = "0.0.1"
 
-globalOfxPluginPath = "/home/juliette/Programmation_compilation/webOpenOFX/TuttleOFX/install/" #install path ?
+globalOfxPluginPath = configParser.get("ofxPath", "globalOfxPluginPath")
 tuttle.core().getPluginCache().addDirectoryToPath(globalOfxPluginPath)
 
 tuttle.core().preload(False)
@@ -17,24 +22,23 @@ plugins = pluginCache.getPlugins()
 
 @app.route('/', methods=['GET'])
 def index():
-  index = "<html><head><title>WebOpenFX - Catalog</title></head><body><h1><center>WebOpenFX - Catalog</center></h1><br/><br/><ul><li>version: " + str(version) + "</li></ul></body>"
-  return index
+    index = "<html><head><title>WebOpenFX - Catalog</title></head><body><h1><center>WebOpenFX - Catalog</center></h1><br/><br/><ul><li>version: " + str(version) + "</li></ul></body>"
+    return index
 
 
-@app.route('/plugins', methods=['GET'])
+@app.route('/plugins/', methods=['GET'])
 def getPlugins():
-  pluginsDescription = {'plugins':[], 'total': len(plugins)}
-  for plugin in plugins:
-    pluginsDescription['plugins'].append(runPlugin.getPluginProperties(plugin))
-
-  return jsonify(**pluginsDescription)
+    pluginsDescription = {'plugins':[], 'total': len(plugins)}
+    for plugin in plugins:
+        pluginsDescription['plugins'].append(analyze.getPluginProperties(plugin))
+    return jsonify(**pluginsDescription)
 
 @app.route('/plugins/<string:pluginId>', methods=['GET'])
 def getPlugin(pluginId):
-  plugin = pluginCache.getPluginById(str(pluginId))
-  pluginDescription = runPlugin.getPluginProperties(plugin)
-  return jsonify(**pluginDescription)
+    plugin = pluginCache.getPluginById(str(pluginId))
+    pluginDescription = analyze.getPluginProperties(plugin)
+    return jsonify(**pluginDescription)
 
 if __name__ == '__main__':
-  # logging.getLogger().setLevel(10)
-  app.run(host='0.0.0.0', port=5010, debug=True)
+    # logging.getLogger().setLevel(10)
+    app.run(host=configParser.get("app:plugin", "host"), port=configParser.getint("app:plugin", "port"), debug=True)
