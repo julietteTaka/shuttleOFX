a3eac73735c306686ff182f539a57a75f76f0890
Armand <armandbiteau@gmail.com>
2015-01-20 20:45:41 +0100

    update front

--- client/static_tmp/scripts/scripts.js
+++ client/static_tmp/scripts/scripts.js
@@ -29,7 +29,16 @@ $("#searchForm").find("#query").blur(
     $(this).parent("form").removeClass("active");
   }
 );
-	
+
+
+
+$('[data-toggle]').on('click', function() {
+  var toggle;
+  toggle = $(this).addClass('active').attr('data-toggle');
+  $(this).siblings('[data-toggle]').removeClass('active');
+  return $('.surveys').removeClass('grid list').addClass(toggle);
+});
+
 
 
 /*
