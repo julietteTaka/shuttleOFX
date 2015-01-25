45952478e9c47847bfcb4c7687bd5632f35a8e60
Armand <armandbiteau@gmail.com>
2015-01-25 17:58:14 +0100

    update front

--- server/getPlugins.py
+++ server/getPlugins.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 from flask import Flask, jsonify
-from pyTuttle import tuttle
-from ofxPlugins import analyze
+# from pyTuttle import tuttle
+# from ofxPlugins import analyze
 import ConfigParser, requests, json
 
 
@@ -12,41 +12,41 @@ configParser.read('configuration.conf')
 
 version = "0.0.1"
 
-globalOfxPluginPath = configParser.get("OFX_PATH", "globalOfxPluginPath")
-tuttle.core().getPluginCache().addDirectoryToPath(globalOfxPluginPath)
+# globalOfxPluginPath = configParser.get("OFX_PATH", "globalOfxPluginPath")
+# tuttle.core().getPluginCache().addDirectoryToPath(globalOfxPluginPath)
 
-tuttle.core().preload(False)
-pluginCache = tuttle.core().getPluginCache()
-plugins = pluginCache.getPlugins()
+# tuttle.core().preload(False)
+# pluginCache = tuttle.core().getPluginCache()
+# plugins = pluginCache.getPlugins()
 
 
 @app.route('/plugins', methods=['GET'])
 def getPlugins():
-    pluginsDescription = {'plugins':[], 'total': 0}
-    for plugin in plugins:
-        pluginsDescription['plugins'].append(analyze.getPluginProperties(plugin))
-    # json_data= open('plugins.json')
-    # data = json.load(json_data)
-    # json_data.close()
+    # pluginsDescription = {'plugins':[], 'total': 0}
+    # for plugin in plugins:
+    #     pluginsDescription['plugins'].append(analyze.getPluginProperties(plugin))
+    json_data= open('plugins.json')
+    data = json.load(json_data)
+    json_data.close()
 
     headers = {
         'content-type': 'application/json'
     }
 
-    # return jsonify(**data)
-    return jsonify(**pluginsDescription)
+    return jsonify(**data)
+    # return jsonify(**pluginsDescription)
 
 
 @app.route('/plugins/<string:pluginId>', methods=['GET'])
 def getPlugin(pluginId):
-    plugin = pluginCache.getPluginById(str(pluginId))
-    pluginDescription = analyze.getPluginProperties(plugin)
-    return jsonify(**pluginDescription)
+    # plugin = pluginCache.getPluginById(str(pluginId))
+    # pluginDescription = analyze.getPluginProperties(plugin)
+    # return jsonify(**pluginDescription)
 
-    # json_data= open('plugin.json')
-    # data = json.load(json_data)
-    # json_data.close()
-    # return jsonify(**data)
+    json_data= open('plugin.json')
+    data = json.load(json_data)
+    json_data.close()
+    return jsonify(**data)
 
 
 
