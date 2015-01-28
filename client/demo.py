d4dfb557de5186cfc781d81edbe403bd69b230f8
Armand <armandbiteau@gmail.com>
2015-01-28 18:32:38 +0100

    add demo.py client

--- client/demo.py
+++ client/demo.py
@@ -1,10 +1,11 @@
-from flask import Flask, request, jsonify
-from flask import render_template, jsonify, json
+from flask import Flask, request, render_template, json, jsonify
+from flask import abort, send_file, Response
 
-import ConfigParser
-import requests
+import ConfigParser, json, requests, uuid, os
+
 
 from client import app
+
 
 def get_resource_as_string(name, charset='utf-8'):
     with app.open_resource(name) as f:
@@ -17,15 +18,62 @@ configParser.read('client/configuration.conf')
 version = "0.0.1"
 
 analyzeRootUri = configParser.get("APP_CLIENT", "analyzeRootUri")
+renderRootUri = configParser.get("APP_CLIENT", "renderRootUri")
+current_render = ""
+_id = 0
+
 
 
 @app.route('/demo')
-def getDemos(pluginId = "tuttle.convolution"):
-    resp = requests.get(analyzeRootUri+"/demo/"+pluginId)
-    return render_template('demo.html', plugin=resp.json())
-
-
 @app.route('/demo/<pluginId>', methods=['GET'])
-def getDemo(pluginId):
-    resp = requests.get(analyzeRootUri+"/demo/"+pluginId)
+def displayIndex(pluginId = None):
+
+    if pluginId == None:
+        pluginId = "tuttle.colorwheel"
+
+    # json_data= open('server/plugin.json')
+    # data = json.load(json_data)
+    # json_data.close()
+    # return render_template('demo.html', plugin=data)
+
+    resp = requests.get(analyzeRootUri+"/plugins/"+pluginId)
     return render_template('demo.html', plugin=resp.json())
+
+
+
+@app.route('/resource/<resourceName>', methods=['GET'])
+def resource(resourceName):
+    resp = requests.get(renderRootUri + "/render/" + current_render['render']['id'] + "/resources/" + resourceName )
+    return Response(resp.content, mimetype="image/png", content_type='image/png')
+
+
+@app.route('/render', methods=['POST'])
+def render():
+    global current_render
+
+    data = request.data
+
+    # print data
+
+    headers = {
+        'Content-Type': 'application/json; charset=utf-8',
+        'Accept-Encoding': 'gzip, deflate'
+    }
+
+    req = requests.post(renderRootUri + "/render", data=data, headers=headers)
+    current_render = req.json()
+    # print req.text
+    return jsonify(**req.json())
+
+
+
+# @app.route('/render/<int:renderId>/resources/<resourceId>', methods=['GET'])
+# def getRenderResource(renderId, resourceId):
+#     req = requests.get(renderRootUri+"/render/"+str(renderId)+"/resources/"+resourceId)
+#     return Response(req.content, mimetype="image/jpeg")
+
+
+# @app.route('/resource/<path:path>', methods=['GET'])
+# def resource(path):
+#   return app.send_static_file('resources/' + path)
+
