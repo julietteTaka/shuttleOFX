1f29d501e8246b2333e3a8ab67091ca57f587711
Armand <armandbiteau@gmail.com>
2015-01-28 18:32:56 +0100

    update plugins & upload client

--- client/plugins.py
+++ client/plugins.py
@@ -7,6 +7,8 @@ import requests
 from client import app
 
 
+
+
 def get_resource_as_string(name, charset='utf-8'):
     with app.open_resource(name) as f:
         return f.read().decode(charset)
@@ -19,13 +21,15 @@ configParser.read('client/configuration.conf')
 version = "0.0.1"
 
 analyzeRootUri = configParser.get("APP_CLIENT", "analyzeRootUri")
+catalogRootUri = configParser.get("APP_CLIENT", "catalogRootUri")
 
 
 
-@app.route('/plugins', methods=['GET', 'POST'])
+
+@app.route('/plugins', methods=['GET'])
 def getPlugins(pluginName=None):
-    resp = requests.get(analyzeRootUri+"/plugins")
-    # resp = requests.get("http://localhost:5004/plugins/")
+
+    resp = requests.get(catalogRootUri+"/plugins", params=request.args)
 
     return render_template('plugins.html', dico=resp.json())
 
@@ -33,8 +37,7 @@ def getPlugins(pluginName=None):
 @app.route('/plugins/<pluginId>', methods=['GET'])
 def getPlugin(pluginId):
     resp = requests.get(analyzeRootUri+"/plugins/"+pluginId)
-    # resp = requests.get("http://localhost:5004/plugins/<pluginId>")
-
+    print resp.text
     return render_template('plugin.html', plugin=resp.json())
 
 
