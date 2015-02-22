7663e9d8531a62bd3505ecd0b9907a6f88b6cf2e
julietteTaka <juliettetuxbelin@gmail.com>
2015-02-22 15:39:03 +0100

    analyzer : asynchrone analyze

--- analyzer/Bundle.py
+++ analyzer/Bundle.py
@@ -9,12 +9,40 @@ from multiprocessing import Process
 from pyTuttle import tuttle
 import plugin
 
+def launchAnalyze(bundle, sharedBundleDatas, bundleExt, bundleBin, bundleId):
+    '''
+    Launches the analyze. Set the process status and fill sharedBundleDatas with the analyzed bundle datas. Delete temporary files and directories created during the archive extraction.
+    '''
+    sharedBundleDatas["status"] = "start extraction"
+
+    if "gzip" == bundleExt.split('/')[1]:
+        bundle.extractDatasAsTar(bundleId, bundleBin)
+    elif "zip" == bundleExt.split('/')[1]:
+        bundle.extractDatasAsZip(bundleId, bundleBin)
+    sharedBundleDatas["status"] = "end extraction"
+
+    sharedBundleDatas["status"] = "start analyze"
+    analyzedBundle = None
+    analyzedBundle = bundle.analyze()
+    sharedBundleDatas["status"] = "end analyze"
+    sharedBundleDatas["datas"] = analyzedBundle
+
+    deleteUnusedFiles(bundle, sharedBundleDatas)
+
+def deleteUnusedFiles(bundle, sharedBundleDatas):
+    sharedBundleDatas["status"] = 'delete ' + "tmp/" + bundle.id + ".tar.gz"
+    os.remove("tmp/" + bundle.id + ".tar.gz")
+    shutil.rmtree("tmp/" + bundle.id)
+
 class Bundle:
     def __init__(self, bundleId):
         self.id = bundleId
         self.path = None
 
     def extractDatasAsTar(self, bundleId, datas):
+        '''
+        Extract bundle as a tar file.
+        '''
         tempFilePath = "tmp/" + bundleId + ".tar.gz"
 
         self.path = "tmp/" + str(bundleId)
@@ -27,20 +55,24 @@ class Bundle:
         tar.extractall(self.path)
 
         tar.close()
-        os.remove(tempFilePath)
+
 
     def extractDatasAsZip(self, bundleId, datas):
+        '''
+        Extract bundle as a zip file.
+        '''
         tempFilePath = "tmp/" + bundleId + ".zip"
         self.path = "tmp/" + str(bundleId)
         f = open(tempFilePath, 'w')
         f.write(datas)
         f.close()
         
-        # TODO
+        # TODO : extract datas
 
-        os.remove(tempFilePath)
-
-    def asyncAnalyze(self, queue):
+    def analyze(self):
+        '''
+        Analyze the bundle an return a description for each plugin.
+        '''
         p = plugin.Plugin()
         pluginCache = tuttle.core().getPluginCache()
         pluginCache.addDirectoryToPath(self.path)
@@ -52,16 +84,4 @@ class Bundle:
         for currentPlugin in plugins:
             pluginsDescription['plugins'].append(p.getPluginProperties(currentPlugin))
 
-        queue.put(pluginsDescription)
-
-    def analyze(self):
-        result_queue = multiprocessing.Queue()
-
-        process = Process(target=self.asyncAnalyze, args=(result_queue,))
-        process.start()
-        analyzedBundle = result_queue.get()
-        process.join()
-
-        shutil.rmtree(self.path)
-
-        return analyzedBundle
+        return pluginsDescription
