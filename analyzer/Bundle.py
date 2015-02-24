7e8d47f61941de600ac3720028d5c33c93016177
julietteTaka <juliettetuxbelin@gmail.com>
2015-02-24 09:49:03 +0100

    analyzer : delete bundle class

--- analyzer/Bundle.py
+++ analyzer/Bundle.py
@@ -3,89 +3,96 @@ import shutil
 import tarfile
 import threading
 import multiprocessing
-from multiprocessing import Process
+import tempfile
 
 
 from pyTuttle import tuttle
 import plugin
 
-def launchAnalyze(bundle, sharedBundleDatas, bundleExt, bundleBin, bundleId):
+
+def extractDatasAsTar(datas, outputPath):
+    '''
+    Extract bundle as a tar file.
+    '''
+    tempFilePath = outputPath + ".tar.gz"
+
+    try:
+        f = open(tempFilePath, 'w')
+        f.write(datas)
+        f.close()
+
+        tar = tarfile.open(tempFilePath, mode='r')
+        tar.extractall(outputPath)
+
+        tar.close()
+    except:
+        print "error while extracting the tar.gz archive"
+    else:
+        os.remove(tempFilePath)
+
+
+def extractDatasAsZip(datas, outputPath):
+    '''
+    Extract bundle as a zip file.
+    '''
+
+    tempFilePath = outputPath + ".zip"
+    try:
+        f = open(tempFilePath, 'w')
+        f.write(datas)
+        f.close()
+    except:
+        print "error while extracting the zip archive"
+    else:
+        os.remove(tempFilePath)
+        raise NotImplementedError()
+        # TODO : extract archive as zip
+
+
+def analyze(pluginPath):
+    '''
+    Analyze the bundle an return a description for each plugin.
+    '''
+
+    p = plugin.Plugin()
+    pluginCache = tuttle.core().getPluginCache()
+    pluginCache.addDirectoryToPath(pluginPath)
+    tuttle.core().preload(False)
+    plugins = pluginCache.getPlugins()
+
+    pluginsDescription = {'plugins':[], 'total': len(plugins)}
+
+    for currentPlugin in plugins:
+        pluginsDescription['plugins'].append(p.getPluginProperties(currentPlugin))
+
+    return pluginsDescription
+
+
+def launchAnalyze(sharedBundleDatas, bundleExt, bundleBin, bundleId):
     '''
     Launches the analyze. Set the process status and fill sharedBundleDatas with the analyzed bundle datas. Delete temporary files and directories created during the archive extraction.
     '''
 
-    sharedBundleDatas["globalStatus"] = "running"
-    sharedBundleDatas["analyzeStatus"] = "waiting"
-    sharedBundleDatas["extractionStatus"] = "running"
+    sharedBundleDatas['globalStatus'] = 'running'
+    sharedBundleDatas['analyzeStatus'] = 'waiting'
+    sharedBundleDatas['extractionStatus'] = 'running'
 
-    if "gzip" == bundleExt.split('/')[1]:
-        bundle.extractDatasAsTar(bundleId, bundleBin)
-    elif "zip" == bundleExt.split('/')[1]:
-        bundle.extractDatasAsZip(bundleId, bundleBin)
-    sharedBundleDatas["extractionStatus"] = "done"
+    bundlePath = 'tmp/' + str(bundleId)
+    os.mkdir(bundlePath)
+
+    if 'gzip' == bundleExt.split('/')[1]:
+        extractDatasAsTar(bundleBin, bundlePath)
+    elif 'zip' == bundleExt.split('/')[1]:
+        extractDatasAsZip(bundleBin, bundlePath)
+    sharedBundleDatas['extractionStatus'] = 'done'
 
     analyzedBundle = None
-    sharedBundleDatas["analyzeStatus"] = "running"
-    analyzedBundle = bundle.analyze()
-    sharedBundleDatas["analyzeStatus"] = "done"
+    sharedBundleDatas['analyzeStatus'] = 'running'
+    analyzedBundle = analyze(bundlePath)
+    sharedBundleDatas['analyzeStatus'] = 'done'
 
-    deleteUnusedFiles(bundle)
+    shutil.rmtree(bundlePath)
 
-    sharedBundleDatas["datas"] = analyzedBundle
-    sharedBundleDatas["globalStatus"] = "done"
+    sharedBundleDatas['datas'] = analyzedBundle
+    sharedBundleDatas['globalStatus'] = 'done'
 
-def deleteUnusedFiles(bundle):
-    os.remove("tmp/" + bundle.id + ".tar.gz")
-    shutil.rmtree("tmp/" + bundle.id)
-
-class Bundle:
-    def __init__(self, bundleId):
-        self.id = bundleId
-        self.path = None
-
-    def extractDatasAsTar(self, bundleId, datas):
-        '''
-        Extract bundle as a tar file.
-        '''
-        tempFilePath = "tmp/" + bundleId + ".tar.gz"
-
-        self.path = "tmp/" + str(bundleId)
-
-        f = open(tempFilePath, 'w')
-        f.write(datas)
-        f.close()
-
-        tar = tarfile.open(tempFilePath, "r")
-        tar.extractall(self.path)
-
-        tar.close()
-
-
-    def extractDatasAsZip(self, bundleId, datas):
-        '''
-        Extract bundle as a zip file.
-        '''
-        tempFilePath = "tmp/" + bundleId + ".zip"
-        self.path = "tmp/" + str(bundleId)
-        f = open(tempFilePath, 'w')
-        f.write(datas)
-        f.close()
-        
-        # TODO : extract datas
-
-    def analyze(self):
-        '''
-        Analyze the bundle an return a description for each plugin.
-        '''
-        p = plugin.Plugin()
-        pluginCache = tuttle.core().getPluginCache()
-        pluginCache.addDirectoryToPath(self.path)
-        tuttle.core().preload(False)
-        plugins = pluginCache.getPlugins()
-
-        pluginsDescription = {'plugins':[], 'total': len(plugins)}
-
-        for currentPlugin in plugins:
-            pluginsDescription['plugins'].append(p.getPluginProperties(currentPlugin))
-
-        return pluginsDescription
