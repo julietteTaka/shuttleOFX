9e23c38733bb43ca14b4e3f054734d845e2609fc
Marc-Antoine Arnaud <marcantoine.arnaud@arkena.com>
2015-02-24 22:00:45 +0100

    add Plugin (new/search)

--- catalog/run.py
+++ catalog/run.py
@@ -19,6 +19,7 @@ config.readfp(open(os.path.join(currentDir, 'catalog.cfg')))
 client = pymongo.MongoClient(config.get('MONGODB', 'hostname'), config.getint('MONGODB', 'port'))
 db = client.wofx
 bundleTable = db.__getattr__(config.get('MONGODB', 'bundleTable'))
+pluginTable = db.__getattr__(config.get('MONGODB', 'pluginTable'))
 
 
 @app.route("/")
@@ -31,7 +32,7 @@ def mongodoc_jsonify(*args, **kwargs):
 
 @app.route("/bundle", methods=["POST"])
 def newBundle():
-    bundleId = request.get_json().get('id', None)
+    bundleId = request.get_json().get('bundleId', None)
     bundleName = request.get_json().get('name', None)
 
     if bundleId == None or bundleName == None:
@@ -41,7 +42,7 @@ def newBundle():
     
     bundleTable.insert(bundle.__dict__)
 
-    requestResult = bundleTable.find_one({"id": bundleId})
+    requestResult = bundleTable.find_one({"bundleId": bundleId})
     return mongodoc_jsonify(requestResult)
 
 
@@ -54,7 +55,7 @@ def getBundles():
 
 @app.route("/bundle/<int:bundleId>")
 def getBundle(bundleId):
-    requestResult = bundleTable.find_one({"id": bundleId})
+    requestResult = bundleTable.find_one({"bundleId": bundleId})
     if requestResult == None:
         abort(404)
 
@@ -63,17 +64,60 @@ def getBundle(bundleId):
 
 @app.route("/bundle/<int:bundleId>", methods=["DELETE"])
 def deleteBundle(bundleId):
-    deleteStatus = bundleTable.remove({"id":bundleId})
+    requestResult = bundleTable.find_one({"bundleId": bundleId})
+    if requestResult == None:
+        abort(404)
+
+    for plugin in requestResult.plugins:
+        deleteStatus = pluginTable.remove({"pluginId":pluginId})
+        if deleteStatus['n'] == 0:
+            abort(404)
+
+    deleteStatus = bundleTable.remove({"bundleId":bundleId})
 
     if deleteStatus['n'] == 0:
         abort(404)
 
     return jsonify(**deleteStatus)
 
-# @app.route("/bundle/<int:bundleId>/plugin" methods=['POST'])
-# def getBundle(bundleId):
-#     requestResult = bundleTable.find_one({"id": bundleId})
-#     return mongodoc_jsonify(requestResult)
+@app.route("/bundle/<int:bundleId>/plugin", methods=['POST'])
+def newPlugin(bundleId):
+    pluginId = request.get_json().get('pluginId', None)
+    pluginName = request.get_json().get('name', None)
+
+    if pluginId == None or pluginName == None:
+        abort(404)
+
+    plugin = Plugin(pluginId, bundleId, pluginName)
+    
+    pluginTable.insert(plugin.__dict__)
+
+    requestResult = pluginTable.find_one({"id": pluginId})
+    return mongodoc_jsonify(requestResult)
+
+
+@app.route("/bundle/<int:bundleId>/plugin")
+def getPlugins(bundleId):
+    count = int(request.args.get('count', 10))
+    skip = int(request.args.get('skip', 0))
+    requestResult = pluginTable.find({"bundleId":bundleId}).limit(count).skip(skip)
+    return mongodoc_jsonify({"plugins":[ result for result in requestResult ]})
+
+@app.route("/plugin/<pluginId>")
+def getAllPlugins():
+    count = int(request.args.get('count', 10))
+    skip = int(request.args.get('skip', 0))
+    requestResult = pluginTable.find().limit(count).skip(skip)
+    return mongodoc_jsonify({"plugins":[ result for result in requestResult ]})
+
+@app.route("/bundle/<int:bundleId>/plugin/<pluginId>")
+@app.route("/plugin/<pluginId>")
+def getPlugin(bundleId):
+    requestResult = pluginTable.find_one({"pluginId": pluginId})
+    if requestResult == None:
+        abort(404)
+
+    return mongodoc_jsonify(requestResult)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5002 ,debug=True)
