9c4619e68469aa2bc97f6567f2f80a0c16baf57c
Marc-Antoine Arnaud <marcantoine.arnaud@arkena.com>
2015-02-24 21:19:37 +0100

    add more parameters on Bundle

--- catalog/Bundle.py
+++ catalog/Bundle.py
@@ -3,8 +3,14 @@
 class Bundle:
     def __init__(self, bundleId, name):
         self.bundleId = bundleId
+        self.userId = 0
+        self.companyId = 0
         self.name = name
         self.description = ""
+        self.shared = false
+        self.contributors = []
+        self.architecture = []
+        self.plugins = []
 
     def setDescription(self, description):
         self.description = description
@@ -12,5 +18,11 @@ class Bundle:
     def printBundle():
         print "Bundle :"
         print "bundleId:", self.bundleId
+        print "userId:", self.userId
+        print "companyId:", self.companyId
         print "name:", self.name
         print "description:", self.description
+        print "shared:", self.shared
+        print "contributors:", self.contributors
+        print "architecture:", self.architecture
+        print "plugins:", self.plugins
