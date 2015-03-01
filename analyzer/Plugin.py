a345246e3b05e0edb1010a86d13c652fd8e257b1
Marc-Antoine Arnaud <marcantoine.arnaud@arkena.com>
2015-03-02 00:35:22 +0100

    remove not extracted informations into a plugin

--- analyzer/Plugin.py
+++ analyzer/Plugin.py
@@ -11,23 +11,12 @@ propTypeToPythonType = {
 
 class Plugin:
     def __init__(self):
-        self.id = ""
-        self.rawIdentifier = ""
-        self.pluginID = ""
-        self.name = ""
-        self.version = ""
+        self.rawIdentifier = None
+        self.name = None
+        self.version = {}
         self.clips = []
         self.parameters = []
         self.properties = []
-        self.tags = ["tagExample"]
-        self.bundleID = ""
-        self.thumbnailPath = ""
-        self.iconPath = ""
-        self.lastAccess = 0
-        self.rate = 0
-        self.presets = []
-        self.defaultImagePaths = []
-        self.sampleImagePaths = []
 
 
     def getDictOfProperty(self, prop):
