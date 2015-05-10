0e812f1afaab9b0e928a7afec2ac5deb42205aa9
Armand <armandbiteau@gmail.com>
2015-05-10 22:07:52 +0200

    fix search engine error

--- client/static_tmp/scripts/search.js
+++ client/static_tmp/scripts/search.js
@@ -10,12 +10,13 @@ $("#searchtrigger").click(function(e){
             if (plugins.plugins.length == 0) {
                 grid.append('<li class="col-md-12"><div class="panel panel-primary no-plugins"><h3>No plugins found</h3></div></li>');
             }else{
-                $.each(plugins, function() {
-                    var id = $(this)[0].pluginId;
-                    var name = $(this)[0].name;
-                    var version = $(this)[0].version.major+"."+$(this)[0].version.minor;
+
+                $.each(plugins.plugins, function(key, val) {
+                    var id = val.pluginId;
+                    var name = val.name;
+                    var version = val.version.major+"."+val.version.minor;
                     var description = "No plugin description";
-                    var prop = $(this)[0].properties;
+                    var prop = val.properties;
                     $.each(prop, function() {
                         var nameProp = $(this)[0].name;
                         if (nameProp == "OfxPropPluginDescription") {
@@ -25,8 +26,8 @@ $("#searchtrigger").click(function(e){
                     });
                     grid.append('<li class="col-md-3"><div class="panel panel-primary"> <a href="/plugin/'+id+'"><div class="plugins-titles"><img src="static/images/logoPad.png" alt="" class="img-responsive plugin-thumbnail"><h3 class="panel-title">'+name+' '+version+'</h3></div></a><div class="plugins-infos"><p>'+description+'</p></div></div></li>');
                 });
+
             };
         });
-    };
-    
+    }; 
 });
