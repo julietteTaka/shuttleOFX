19dfd455fa53bea9c959f0d789297f0b673fd68a
Hugo Garrido y Saez <garrido.saez.hugo@gmail.com>
2015-05-10 19:04:44 +0200

    script in sperate file and cleaning

--- client/static_tmp/scripts/dropzone-config.js
+++ client/static_tmp/scripts/dropzone-config.js
@@ -1,149 +1,58 @@
-$(document).ready(function(){
-    console.log("f");
-    Dropzone.autoDiscover = false;
-    
-    var bundleId = undefined;
-    //var bundleId;
-    console.log("START : bundleId : "+bundleId);
 
-    var myDropzone = undefined;
-    $("#droparea").hide();
-    $('#fileSubmit').hide();
+Dropzone.autoDiscover = false;
 
-    $(function(){
-        var wofxDropzone = new Dropzone("#droparea", {
-            url: "undefinedUrl",
-            acceptedFiles: "application/zip, application/x-tar, application/gzip, application/x-gzip", //This is a comma separated list of mime types or file extensions.Eg.: image/*,application/pdf,.psd.
-            maxFilesize: 1024, // in MB
-            paramName: "file" // The name that will be used to transfer the file
-        });
+var bundleId = undefined;
+var myDropzone = undefined;
 
-        wofxDropzone.options.previewTemplate = '\
-            <div class="dz-preview dz-file-preview">\
-              <div class="dz-details">\
-              <div class="dz-filename"><span data-dz-name></span></div>\
-              <div class="dz-size" data-dz-size></div>\
-              <img data-dz-thumbnail />\
+$("#droparea").hide();
+$('#fileSubmit').hide();
+
+$(function(){
+    var wofxDropzone = new Dropzone("#droparea", {
+        url: "undefinedUrl",
+        acceptedFiles: "application/zip, application/x-tar, application/gzip, application/x-gzip",
+        maxFilesize: 1024,
+        paramName: "file"
+    });
+
+    wofxDropzone.options.previewTemplate = '\
+        <div class="dz-preview dz-file-preview">\
+            <div class="dz-details">\
+                <div class="dz-filename"><span data-dz-name></span></div>\
+                <div class="dz-size" data-dz-size></div>\
+                <img data-dz-thumbnail />\
             </div>\
-            <div class="dz-progress"><span class="dz-upload" data-dz-uploadprogress></span></div>\
-            <div class="dz-error-message"><span data-dz-errormessage></span></div>\
-            </div>';
+        <div class="dz-progress"><span class="dz-upload" data-dz-uploadprogress></span></div>\
+        <div class="dz-error-message"><span data-dz-errormessage></span></div>\
+        </div>';
 
-        $('#fileSubmit').click(function(){
-            console.log("upload to :" + wofxDropzone.options.url);
-            console.log(wofxDropzone);
+    $('#fileSubmit').click(function(){
+        wofxDropzone.options.headers={"Content-Type": wofxDropzone.files[0].type};
+         wofxDropzone.processQueue();
+    });
 
-            wofxDropzone.options.headers={"Content-Type": wofxDropzone.files[0].type};
-            wofxDropzone.processQueue(); //processes the queue
-        });
+    $('#metaBundle').submit(function(event, bundleId){
+      
+        bundleName = $("#bundleName").val();
+        bundleDescription = $("#bundleDescription").val();
+        var userId = $("#createBundle").attr("attr-id");
 
-        $('#createBundle').click(function(event, bundleId){
+        $.ajax({
+            type : 'POST',
+            url : '/bundle',
+            dataType: 'json',
+            data : {'bundleName' : bundleName, 'bundleDescription' : bundleDescription, 'userId' : userId}
+        }).done(function(data, bundleId){
             
-            console.log("submit");
-            bundleName = $("#bundleName").val();
-            bundleDescription = $("#bundleDescription").val();
-            console.log(bundleDescription);
+            $("#droparea").show();
+            $('#fileSubmit').show();
+            $("#createBundle").hide();
 
-            $.ajax({
-                type : 'POST',
-                url : '/bundle',
-                data : {'bundleName' : bundleName, 'bundleDescription' : bundleDescription}
-            }).done(function(data, bundleId){
-                
-                $("#droparea").show();
-                console.log("DROPZONE IS READY, BUNDLEID IS GENERATED !")
-
-                jsonData = JSON.parse(data);
-                bundleId = jsonData.b[0].bundleId;
-                console.log("MIDDLE : "+bundleId);
-
-                var bundleURI = "/bundle/"+bundleId+"/upload";
-
-                wofxDropzone.options.url = bundleURI;
-
-                $('#fileSubmit').show();
-            });
-
-            console.log("END : bundleId : "+bundleId);
-            console.log("---------------------------------");
-            event.preventDefault();
-            return false;
+            bundleId = data.bundleId;
+            var bundleURI = "/bundle/"+bundleId+"/archive";
+            wofxDropzone.options.url = bundleURI;
         });
+        event.preventDefault();
     });
 });
 
-
-
-
-
-
-
-
-/*  var bundleId = undefined;
-  var myDropzone = undefined;
-  console.log("hello");
-  $("#droparea").hide();
-  $('#fileSubmit').hide();
-
-  $(function(){
-    var wofxDropzone = new Dropzone("#droparea", {
-      url: "undefinedUrl",
-      acceptedFiles: "application/zip, application/x-tar, application/gzip, application/x-gzip", 
-      maxFilesize: 1024,
-      paramName: "file"
-  } );
-  wofxDropzone.autoDiscover = false;
-  wofxDropzone.options.previewTemplate = '\
-      <div class="dz-preview dz-file-preview">\
-        <div class="dz-details">\
-        <div class="dz-filename"><span data-dz-name></span></div>\
-        <div class="dz-size" data-dz-size></div>\
-        <img data-dz-thumbnail />\
-      </div>\
-      <div class="dz-progress"><span class="dz-upload" data-dz-uploadprogress></span></div>\
-      <div class="dz-error-message"><span data-dz-errormessage></span></div>\
-      </div>';
-
-
-  $('#fileSubmit').click(function(){
-    console.log("upload to :" + wofxDropzone.options.url);
-    console.log(wofxDropzone);
-
-    wofxDropzone.options.headers={"Content-Type": wofxDropzone.files[0].type};
-    wofxDropzone.processQueue();
-  });
-
-  $('#metaBundle').submit(function(event, bundleId){
-      console.log("submit");
-      bundleName = $("#bundleName").val();
-      bundleDescription = $("#bundleDescription").val();
-      console.log(bundleDescription);
-
-      $.ajax({
-          type : 'POST',
-          url : '/bundle',
-          data : {'bundleName' : bundleName, 'bundleDescription' : bundleDescription}
-      }).done(function(data, bundleId){
-          $("#droparea").show();
-          console.log("DROPZONE IS READY, BUNDLEID IS GENERATED !")
-
-          jsonData = JSON.parse(data);
-          bundleId = jsonData.b[0].bundleId;
-          console.log("MIDDLE : "+bundleId);
-
-          var bundleURI = "/bundle/"+bundleId+"/upload";
-
-          wofxDropzone.options.url = bundleURI;
-
-          $('#fileSubmit').show();
-          // dict[url] = "/file/post";
-          // $("div#myId").dropzone(dict);
-    });
-
-    console.log("END : bundleId : "+bundleId);
-    console.log("---------------------------------");
-    event.preventDefault();
-
-  });
-});*/
-//});
