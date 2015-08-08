a3af27a04f4f92cdc3aeb420f211f1589555256a
julietteTaka <juliettetuxbelin@gmail.com>
2015-08-08 19:57:23 +0200

    [client] display the search bar on plugin catalog only

--- shuttleofx_client/static_tmp/scripts/search.js
+++ shuttleofx_client/static_tmp/scripts/search.js
@@ -1,4 +1,11 @@
 //search
+
+$(document).ready(function() {
+    console.log("display");
+    //display search bar
+    $("#searchForm").css({"display":"block"});
+});
+
 $("#searchtrigger").click(function(e){
     var keyword = $("#searchquery").val();    
     if (keyword) {
