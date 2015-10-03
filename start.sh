66187933bc7f3736ea96705e9a5a4c8ebd514c1c
julietteTaka <juliettetuxbelin@gmail.com>
2015-10-04 01:25:28 +0200

    start.sh: use wait command

--- start.sh
+++ start.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
 echo "Starting mongodb"
-/opt/mongodb/bin/mongod --dbpath /opt/mongo-data > /opt/logs/mongo.log 2>&1 &
+(/opt/mongodb/bin/mongod --dbpath /opt/mongo-data > /opt/logs/mongo.log 2>&1) &
 
-python ${SHUTTLEOFX_DEV}/shuttleofx_analyser/__init__.py > /opt/logs/analyser.log 2>&1 &
-python ${SHUTTLEOFX_DEV}/shuttleofx_render/__init__.py > /opt/logs/render.log 2>&1 &
-python ${SHUTTLEOFX_DEV}/shuttleofx_catalog/__init__.py > /opt/logs/catalog.log 2>&1 &
-python ${SHUTTLEOFX_DEV}/shuttleofx_client/__init__.py > /opt/logs/client.log 2>&1
+echo "Starting Analyzer"
+(python ${SHUTTLEOFX_DEV}/shuttleofx_analyser/views.py > /opt/logs/analyser.log 2>&1) &
+echo "Starting Render"
+(python ${SHUTTLEOFX_DEV}/shuttleofx_render/views.py > /opt/logs/render.log 2>&1) &
+echo "Starting Catalog"
+(python ${SHUTTLEOFX_DEV}/shuttleofx_catalog/views.py > /opt/logs/catalog.log 2>&1) &
+echo "Starting Client"
+(python ${SHUTTLEOFX_DEV}/shuttleofx_client/views.py > /opt/logs/client.log 2>&1) &
+echo "END"
+
+wait
+
