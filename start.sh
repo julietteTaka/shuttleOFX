386595aef8b48518bae7b6d4dd13b4a4ed76ecb2
julietteTaka <juliettetuxbelin@gmail.com>
2015-10-20 23:17:58 +0200

    Ensure mongodb restart

--- start.sh
+++ start.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
 echo "Starting mongodb"
-(/opt/mongodb/bin/mongod --dbpath /opt/mongo-data > /opt/logs/mongo.log 2>&1) &
+if [ -s /opt/mongo-data/mongod.lock ];
+	then
+		echo "Data base needs to be repaired"
+		rm -f  /opt/mongo-data/mongod.lock
+		/opt/mongodb/bin/mongod --repair --dbpath /opt/mongo-data
+fi
+wait
 
+/opt/mongodb/bin/mongod --dbpath /opt/mongo-data > /opt/logs/mongo.log 2>&1 &
 echo "Starting Analyzer"
-(python ${SHUTTLEOFX_DEV}/shuttleofx_analyser/views.py > /opt/logs/analyser.log 2>&1) &
+python ${SHUTTLEOFX_DEV}/shuttleofx_analyser/views.py > /opt/logs/analyser.log 2>&1 &
 echo "Starting Render"
-(python ${SHUTTLEOFX_DEV}/shuttleofx_render/views.py > /opt/logs/render.log 2>&1) &
+python ${SHUTTLEOFX_DEV}/shuttleofx_render/views.py > /opt/logs/render.log 2>&1 &
 echo "Starting Catalog"
-(python ${SHUTTLEOFX_DEV}/shuttleofx_catalog/views.py > /opt/logs/catalog.log 2>&1) &
+python ${SHUTTLEOFX_DEV}/shuttleofx_catalog/views.py > /opt/logs/catalog.log 2>&1 &
 echo "Starting Client"
-(python ${SHUTTLEOFX_DEV}/shuttleofx_client/views.py > /opt/logs/client.log 2>&1) &
+python ${SHUTTLEOFX_DEV}/shuttleofx_client/views.py > /opt/logs/client.log 2>&1 &
 echo "END"
 
 wait
