3b6f1739137d98dbaa9dbf91b1ab5f3a0c6fc2d3
Christophe AC <assante.christophe@laposte.net>
2015-12-12 22:28:06 +0100

    Implementation of pagination for the plugins catalog
    - user can choose how many plugins to show per page
    - user can navigate between the catalog pages with the next and previous
    buttons

--- shuttleofx_client/static_tmp/scripts/counter.js
+++ shuttleofx_client/static_tmp/scripts/counter.js
@@ -1,66 +1,50 @@
-$('select#pageSize').change(function(){
-    var size = $('select#pageSize').val();
-    $.get('/plugin/count?count='+size, function(data) {
-        var plugins = data;
-        var grid = $("#plugins ul.surveys");
-        grid.html("");
-        $.each(plugins.plugins, function(key, val) {
-            var id = val.pluginId;
-            var name = val.label;
-            var description = "No plugin description";
-            var prop = val.properties;
-            if("OfxPropPluginDescription" in val.properties)
-                description = val.properties.OfxPropPluginDescription.value
-            grid.append('<li class="col-md-3"><div class="panel panel-primary"> <a href="/plugin/'+id+'"><div class="plugins-titles"><img src="static/images/logoPad.png" alt="" class="img-responsive plugin-thumbnail"><h3 class="panel-title">'+name+'</h3></div></a><div class="plugins-infos"><p>'+description+'</p></div></div></li>');
-        });
-    });
-});
-var skip = 0;
-if (skip <= 0) {
+var currentUrl = window.location.href;
+var decomposedUrl = currentUrl.split("?");
+
+var skip;
+var count;
+
+var totalPlugins = $("#totalPlugins").val();
+
+if (decomposedUrl.length < 2) {
+  count = 10;
+  skip = 1;
+}
+else {
+  decomposedUrl = decomposedUrl[1].split("&");
+  count = decomposedUrl[0].split("=")[1];
+  skip = decomposedUrl[1].split("=")[1];
+}
+
+$('select#pageSize').val(count);
+
+if (skip <= 1) {
     $('#previous').addClass('disabled');
-};
-$('#next a').click(function(){
-    $('#previous').removeClass('disabled');
-    var size = $('select#pageSize').val();
-    skip = skip + parseInt(size); 
-    // console.log("skip "+ skip);
-    // console.log("size "+ size);
-    $.get('/plugin/count?count='+size+'&skip='+skip, function(data) {
-        var plugins = data;
-        var grid = $("#plugins ul.surveys");
-        grid.html("");
-        $.each(plugins.plugins, function(key, val) {
-            var id = val.pluginId;
-            var name = val.label;
-            var description = "No plugin description";
-            var prop = val.properties;
-                if("OfxPropPluginDescription" in val.properties)
-                        description = val.properties.OfxPropPluginDescription.value
-            grid.append('<li class="col-md-3"><div class="panel panel-primary"> <a href="/plugin/'+id+'"><div class="plugins-titles"><img src="static/images/logoPad.png" alt="" class="img-responsive plugin-thumbnail"><h3 class="panel-title">'+name+'</h3></div></a><div class="plugins-infos"><p>'+description+'</p></div></div></li>');
-        });
-    });
+}
+else if (skip == Math.ceil(totalPlugins/count)) {
+  $('#next').addClass('disabled');
+}
+
+$('select#pageSize').change(function(){
+  count = $('select#pageSize').val();
+  if (skip > Math.ceil(totalPlugins/$('select#pageSize').val())) {
+    skip = Math.ceil(totalPlugins/$('select#pageSize').val());
+  }
+  window.location.href = "/plugin?count=" + count + "&skip=" + skip;
 });
-$('#previous a').click(function(){
-    var size = $('select#pageSize').val();
-    skip = skip - parseInt(size); 
-    if (skip <= 0) {
-        skip=0;
-        $('#previous').addClass('disabled');
-    };
-    // console.log("skip "+ skip);
-    // console.log("size "+ size);
-    $.get('/plugin/count?count='+size+'&skip='+skip, function(data) {
-        var plugins = data;
-        var grid = $("#plugins ul.surveys");
-        grid.html("");
-        $.each(plugins.plugins, function(key, val) {
-            var id = val.pluginId;
-            var name = val.label;
-            var description = "No plugin description";
-            var prop = val.properties;
-            if("OfxPropPluginDescription" in val.properties)
-                description = val.properties.OfxPropPluginDescription.value
-            grid.append('<li class="col-md-3"><div class="panel panel-primary"> <a href="/plugin/'+id+'"><div class="plugins-titles"><img src="static/images/logoPad.png" alt="" class="img-responsive plugin-thumbnail"><h3 class="panel-title">'+name+'</h3></div></a><div class="plugins-infos"><p>'+description+'</p></div></div></li>');
-        });
-    });
+
+$('#next a').click(function(event){
+  event.preventDefault();
+  if (!$("#next").hasClass("disabled")) {
+    skip ++;
+    window.location.href = "/plugin?count=" + count + "&skip=" + skip;
+  }
+});
+
+$('#previous a').click(function(event){
+  event.preventDefault();
+  if (!$("#previous").hasClass("disabled")) {
+    skip --;
+    window.location.href = "/plugin?count=" + count + "&skip=" + skip;
+  }
 });
