2195d7b23afa549bf475db46500a2140797a7f13
Olivier-Daire <daire.olivier@gmail.com>
2016-01-19 10:07:35 +0100

    Detect Google and Github API errors using HTTP status code

--- shuttleofx_client/userManager.py
+++ shuttleofx_client/userManager.py
@@ -4,18 +4,25 @@ import logging
 
 def getUser():
 	if 'google_token' in session:
-		googleUser = config.google.get('userinfo').data
-		user = {'name': googleUser.get('name'), 'picture': googleUser.get('picture')}
-		return user
-	elif 'github_token' in session:
-		githubUser = config.github.get( '?access_token=' + str(session['github_token'][0]) ).data
-
-		if githubUser.get('name') is None:
-			name = githubUser.get('login')
+		resp = config.google.get('userinfo')
+		if resp.status is not 200:
+			logging.error("Bad request Google authentication API : " + resp.data)
 		else:
-			name = githubUser.get('name')
+			googleUser = resp.data
+			user = {'name': googleUser.get('name'), 'picture': googleUser.get('picture')}
+			return user
+	elif 'github_token' in session:
+		resp = config.github.get( '?access_token=' + str(session['github_token'][0]) )
+		if resp.status is not 200:
+			logging.error("Bad request Github authentication API : " + str(resp.data))
+		else:
+			githubUser = resp.data
+			if githubUser.get('name') is None:
+				name = githubUser.get('login')
+			else:
+				name = githubUser.get('name')
 
-		user = {'name': name, 'picture': githubUser.get('avatar_url')}
-		return user
+			user = {'name': name, 'picture': githubUser.get('avatar_url')}
+			return user
 	else:
 		return None
