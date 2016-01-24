d9b1baef2f73692fa1d5e7f29d3d8bf3a2fa58fe
Olivier-Daire <daire.olivier@gmail.com>
2016-01-24 14:23:26 +0100

    Improve pagination to work with categories

--- shuttleofx_client/static_tmp/scripts/counter.js
+++ shuttleofx_client/static_tmp/scripts/counter.js
@@ -6,30 +6,33 @@ var count = 10; // number of plugin per page default = 10
 var skip = 1; // number of current page default = 1
 var totalPlugins = $("#totalPlugins").val(); // number of plugins return by the search 
 
+var page = decomposedUrl[0].split("/"); // page plugin or category
+page = page[ page.length - 1 ];
+
 // get parameters in url and put them in count, skip and search
 if (typeof decomposedUrl[1] != "undefined") {
   decomposedUrl = decomposedUrl[1].split("&");
   var params = [];
 
   for (var i = 0; i < decomposedUrl.length; i++) {
-    tmp = decomposedUrl[i].split("=");
-    key = tmp[0];
-    val = tmp[1];
+    var tmp = decomposedUrl[i].split("=");
+    var key = tmp[0];
+    var val = tmp[1];
     params[key] = val;
   };
 
   if (typeof params["search"] != "undefined") search = params["search"];
   if (typeof params["skip"] != "undefined") skip = params["skip"];
   if (typeof params["count"] != "undefined") count = params["count"];
-};
+}
 
 // Update cookie with new parameters
-cookieManager({"search": search, "count": count, "skip":skip});
+cookieManager({"page": page, "search": search, "count": count, "skip":skip});
 
 // Change number of plugin per page
 $('select#pageSize').change(function(){
   count = $('select#pageSize').val();
-  maxPage = Math.ceil(totalPlugins/$('select#pageSize').val());
+  var maxPage = Math.ceil(totalPlugins/$('select#pageSize').val());
   if (skip > maxPage) {
     skip = maxPage;
   }
@@ -37,10 +40,10 @@ $('select#pageSize').change(function(){
   cookieManager({"count": count, "skip": skip});
 
   if (typeof search == "undefined") {
-    window.location.href = "/plugin?count=" + count + "&skip=" + skip;
+    window.location.href = "/" + page + "?count=" + count + "&skip=" + skip;
   }
   else {
-    window.location.href = "/plugin?search=" + search + "&count=" + count + "&skip=" + skip;
+    window.location.href = "/" + page + "?search=" + search + "&count=" + count + "&skip=" + skip;
   }
 });
 
@@ -49,7 +52,7 @@ $('select#pageSize').change(function(){
 // skip --> on which page is the user
 function cookieManager(value){
   if (!Cookies.get("user_sorting_prefs")) {
-      Cookies.set("user_sorting_prefs", value, { expires: 30, path: "/" });
+    Cookies.set("user_sorting_prefs", value, { expires: 30, path: "/" });
   } else {
     Cookies.set("user_sorting_prefs", value);
   }
@@ -59,4 +62,4 @@ function cookieManager(value){
 $("li.disabled a").click(function(e){
   e.preventDefault();
   return;
-})
+});
